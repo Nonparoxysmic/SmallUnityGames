d7ab37d80d63b8830606821677d8bc93e49b65da
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2020-08-20 21:50:03 -0500

    Simple sprite work

---
+++ TicTacToe/Assets/Scripts/ToggleOnClick.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ToggleOnClick : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public Sprite xSprite;
+    public Sprite oSprite;
+    int currentSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = null;
+        currentSprite = 0;
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        if (currentSprite == 0)
+        {
+            sr.sprite = xSprite;
+            currentSprite = 1;
+        }
+        else if (currentSprite == 1)
+        {
+            sr.sprite = oSprite;
+            currentSprite = 2;
+        }
+        else
+        {
+            sr.sprite = null;
+            currentSprite = 0;
+        }
+    }
+}
