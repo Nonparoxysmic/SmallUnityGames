8d3a9f050f650cb3399e0c87abaedd04c39602c8
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2020-08-27 23:17:25 -0500

    Improved a few things

--- TicTacToe/Assets/Scripts/BoxState.cs
+++ TicTacToe/Assets/Scripts/BoxState.cs
@@ -1,15 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class BoxState : MonoBehaviour
 {
-    public Animator animator;
-    public GameObject gameGrid;
+    Animator animator;
+    GameObject gameGrid;
     GridState gs;
+    int gridX;
+    int gridY;
     [HideInInspector] public Letter CurrentLetter { get; private set; }
 
     void Start()
     {
         CurrentLetter = Letter.Blank;
+        gridX = (int)Math.Round(this.transform.position.x / 2.0) + 1;
+        gridY = (int)Math.Round(this.transform.position.y / 2.0) + 1;
+        animator = this.GetComponent<Animator>();
+        gameGrid = this.transform.parent.gameObject;
         gs = gameGrid.GetComponent<GridState>();
     }
 
@@ -17,6 +24,6 @@ public class BoxState : MonoBehaviour
     {
         CurrentLetter = letter;
         animator.SetInteger("Letter", (int)CurrentLetter);
-        gs.UpdateGridState();
+        gs.UpdateGridState(gridX, gridY, CurrentLetter);
     }
 }
--- TicTacToe/Assets/Scripts/GridState.cs
+++ TicTacToe/Assets/Scripts/GridState.cs
@@ -3,26 +3,16 @@
 public class GridState : MonoBehaviour
 {
     Letter[,] gridLetters;
-    public BoxState[] boxStates;
     public GameObject testLine;
 
     void Start()
     {
         gridLetters = new Letter[3, 3];
-        UpdateGridState();
     }
 
-    public void UpdateGridState()
+    public void UpdateGridState(int boxX, int boxY, Letter newLetter)
     {
-        gridLetters[0, 0] = boxStates[6].CurrentLetter;
-        gridLetters[1, 0] = boxStates[7].CurrentLetter;
-        gridLetters[2, 0] = boxStates[8].CurrentLetter;
-        gridLetters[0, 1] = boxStates[3].CurrentLetter;
-        gridLetters[1, 1] = boxStates[4].CurrentLetter;
-        gridLetters[2, 1] = boxStates[5].CurrentLetter;
-        gridLetters[0, 2] = boxStates[0].CurrentLetter;
-        gridLetters[1, 2] = boxStates[1].CurrentLetter;
-        gridLetters[2, 2] = boxStates[2].CurrentLetter;
+        gridLetters[boxX, boxY] = newLetter;
 
         if ((gridLetters[0, 0] != Letter.Blank) && (gridLetters[0, 0] == gridLetters[1, 0]) && (gridLetters[0, 0] == gridLetters[2, 0]))
         {
@@ -32,5 +22,5 @@ public class GridState : MonoBehaviour
         {
             testLine.SetActive(false);
         }
-    }
+    }    
 }
