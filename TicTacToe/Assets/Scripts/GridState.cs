b5575acf1283fab45f636d918691bdc1ae081742
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2020-08-31 22:39:51 -0500

    Finished basic line drawing

--- TicTacToe/Assets/Scripts/GridState.cs
+++ TicTacToe/Assets/Scripts/GridState.cs
@@ -3,14 +3,17 @@
 public class GridState : MonoBehaviour
 {
     [HideInInspector] public Letter[,] GridLetters { get; private set; }
+    LineDrawer ld;
 
     void Start()
     {
         GridLetters = new Letter[3, 3];
+        ld = GetComponent<LineDrawer>();
     }
 
     public void UpdateGridState(int boxX, int boxY, Letter newLetter)
     {
         GridLetters[boxX, boxY] = newLetter;
+        ld.DrawLines();
     }    
 }
--- TicTacToe/Assets/Scripts/LineDrawer.cs
+++ TicTacToe/Assets/Scripts/LineDrawer.cs
@@ -13,12 +13,54 @@ public class LineDrawer : MonoBehaviour
         lines = new List<GameObject>();
     }
 
-    void CreateLine()
+    public void DrawLines()
+    {
+        Letter[,] gridLetters = gs.GridLetters;
+        ClearAllLines();
+        // Checking the rows:
+        for (int row = 0; row < 3; row++)
+        {
+            if ((gridLetters[0, row] != Letter.Blank) && (gridLetters[0, row] == gridLetters[1, row]) && (gridLetters[0, row] == gridLetters[2, row]))
+            {
+                CreateLine(-3.5f, (row - 1) * 2, 3.5f, (row - 1) * 2);
+            }
+        }
+        // Checking the columns:
+        for (int col = 0; col < 3; col++)
+        {
+            if ((gridLetters[col, 0] != Letter.Blank) && (gridLetters[col, 0] == gridLetters[col, 1]) && (gridLetters[col, 0] == gridLetters[col, 2]))
+            {
+                CreateLine((col - 1) * 2, -3.5f, (col - 1) * 2, 3.5f);
+            }
+        }
+        // Checking the diagonals:
+        if ((gridLetters[0, 0] != Letter.Blank) && (gridLetters[0, 0] == gridLetters[1, 1]) && (gridLetters[0, 0] == gridLetters[2, 2]))
+        {
+            CreateLine(-3.5f, -3.5f, 3.5f, 3.5f);
+        }
+        if ((gridLetters[0, 2] != Letter.Blank) && (gridLetters[0, 2] == gridLetters[1, 1]) && (gridLetters[0, 2] == gridLetters[2, 0]))
+        {
+            CreateLine(-3.5f, 3.5f, 3.5f, -3.5f);
+        }
+    }
+
+    void ClearAllLines()
+    {
+        foreach (GameObject line in lines)
+        {
+            Destroy(line);
+        }
+        lines.Clear();
+    }
+
+    void CreateLine(float x0, float y0, float x1, float y1)
     {
         GameObject newLine = Instantiate(lineBase);
         newLine.name = "Line" + lines.Count.ToString();
-        
-
+        Vector3[] positions = new Vector3[2];
+        positions[0] = new Vector3(x0, y0, 0);
+        positions[1] = new Vector3(x1, y1, 0);
+        newLine.GetComponent<LineRenderer>().SetPositions(positions);
         lines.Add(newLine);
     }
 }
