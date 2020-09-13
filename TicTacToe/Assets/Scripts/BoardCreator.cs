7c06fbb66d0b5e14737addd1ea51844e866c2dbd
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2020-09-12 22:34:48 -0500

    Started redoing everything

---
+++ TicTacToe/Assets/Scripts/BoardCreator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class BoardCreator : MonoBehaviour
+{
+    [SerializeField] GameObject boxPrefab;
+    
+    void Start()
+    {
+        if (boxPrefab == null)
+        {
+            Debug.LogError("Box Prefab reference is not set in the Inspector.");
+        }
+    }
+
+    public void CreateGameBoard(int width, int height, int victoryNumber)
+    {
+        if ((width < 1) || (height < 1) || (victoryNumber < 1))
+        {
+            Debug.LogError("Invalid parameters in CreateGameBoard().");
+            return;
+        }
+
+        int baseX = (int)Math.Round(gameObject.transform.position.x) + 1 - width;
+        int baseY = (int)Math.Round(gameObject.transform.position.y) + 1 - height;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GameObject createdBox = Instantiate(boxPrefab, new Vector3(baseX + x * 2, baseY + y * 2, 0), Quaternion.identity, gameObject.transform);
+                createdBox.name = "Box (" + x + "," + y + ")";
+            }
+        }
+    }
+}
---
+++ TicTacToe/Assets/Scripts/TESTBoardCreator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class TESTBoardCreator : MonoBehaviour
+{
+    [SerializeField] GameObject gameBoardPrefab;
+    public int boardWidth = 3;
+    public int boardHeight = 3;
+    public int victoryNum = 3;
+
+    void Start()
+    {
+        GameObject newObject = Instantiate(gameBoardPrefab, new Vector3(0, 0, 100), Quaternion.identity);
+        newObject.name = "Game Board Test";
+        newObject.GetComponent<BoardCreator>().CreateGameBoard(boardWidth, boardHeight, victoryNum);
+    }
+}
