7b015c06c93d45df9d614a2f65568c01cc51a2ed
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2020-09-15 23:15:18 -0500

    Fixed some coordinates again

--- TicTacToe/Assets/Scripts/V2/TESTBoardCreator.cs
+++ TicTacToe/Assets/Scripts/V2/TESTBoardCreator.cs
@@ -10,7 +10,9 @@ public class TESTBoardCreator : MonoBehaviour
 
     void Start()
     {
-        GameObject newObject = Instantiate(gameBoardPrefab, new Vector3(0, 0, 100), Quaternion.identity, gameObject.transform.parent.transform);
+        int baseX = (int)Math.Round(gameObject.transform.parent.transform.position.x);
+        int baseY = (int)Math.Round(gameObject.transform.parent.transform.position.y);
+        GameObject newObject = Instantiate(gameBoardPrefab, new Vector3(baseX, baseY, 100), Quaternion.identity, gameObject.transform.parent.transform);
         newObject.name = "Game Board Test";
         newObject.GetComponent<BoardCreator>().CreateGameBoard(boardWidth, boardHeight, victoryNum);
     }
