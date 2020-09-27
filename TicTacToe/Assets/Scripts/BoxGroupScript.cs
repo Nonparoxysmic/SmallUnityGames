47b8fdd4d35c38930569304cdca86238a9d4ddc3
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2020-09-26 22:54:40 -0500

    Started on scripts for boxes

---
+++ TicTacToe/Assets/Scripts/BoxGroupScript.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class BoxGroupScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+}
---
+++ TicTacToe/Assets/Scripts/BoxScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class BoxScript : MonoBehaviour
+{
+    Animator animator;
+    [HideInInspector] public int boxNumber;
+    Letter currentLetter;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Box " + boxNumber + " clicked.");
+    }
+
+    public void SetLetter(Letter newLetter)
+    {
+        currentLetter = newLetter;
+    }
+}
