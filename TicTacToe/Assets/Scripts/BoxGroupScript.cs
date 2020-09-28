b3b7ed7ba7f06ed61600037ae60328b6d197fda8
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2020-09-27 23:02:22 -0500

    Some box work

--- TicTacToe/Assets/Scripts/BoxGroupScript.cs
+++ TicTacToe/Assets/Scripts/BoxGroupScript.cs
@@ -3,8 +3,24 @@ using UnityEngine;
 
 public class BoxGroupScript : MonoBehaviour
 {
+    [SerializeField] GameObject boxPrefab;
+    GameObject[] boxes;
+
     void Start()
     {
-        
+        if (boxPrefab == null)
+        {
+            Debug.LogError(gameObject.name + ": Prefab reference not set in the Inspector.");
+            boxPrefab = new GameObject();
+        }
+        boxes = new GameObject[9];
+        for (int i = 0; i < 9; i++)
+        {
+            float x = (i % 3 - 1) * 2 + gameObject.transform.position.x;
+            float y = ((i - i % 3) / 3 - 1) * -2 + gameObject.transform.position.y;
+            boxes[i] = Instantiate(boxPrefab, new Vector2(x, y), Quaternion.identity, gameObject.transform);
+            boxes[i].name = "Box " + i;
+            boxes[i].GetComponent<BoxScript>().boxNumber = i;
+        }
     }
 }
---
+++ TicTacToe/Assets/Scripts/BoxUpdate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class BoxUpdate
+{
+    int boxNumber;
+    Letter newLetter;
+
+    public BoxUpdate(int num, Letter let)
+    {
+        boxNumber = num;
+        newLetter = let;
+    }
+}
