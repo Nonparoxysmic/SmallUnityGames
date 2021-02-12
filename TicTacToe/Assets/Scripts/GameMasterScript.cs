f6b3bd2e55b37b4b59589ed37140335777cab165
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-02-11 21:03:09 -0600

    Add simple computer player logic

--- TicTacToe/Assets/Scripts/GameMasterScript.cs
+++ TicTacToe/Assets/Scripts/GameMasterScript.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameMasterScript : MonoBehaviour
 {
     public BoxClickedEvent boxClicked;
     public BoxUpdatedEvent boxUpdated;
+    [SerializeField]
+    GameDifficulty difficulty;
     int numberOfMoves;
     Letter playerLetter;
     Letter computerLetter;
@@ -54,22 +57,110 @@ public class GameMasterScript : MonoBehaviour
 
     int ComputerMoveBox()
     {
-        //if ((int)gd > 0) return -1; // Temporary until difficulty implemented
-
         if (numberOfMoves >= 9) return -1;
+        if ((int)difficulty == 0) return RandomMove();
+        return WinOrRandomMove();
+    }
 
+    int RandomMove()
+    {
         int randomBlankBoxNumber;
-        do
-        {
+        do {
             randomBlankBoxNumber = UnityEngine.Random.Range(0, 9);
         } while (letterGrid[randomBlankBoxNumber] != Letter.Blank);
         return randomBlankBoxNumber;
     }
 
-    //int FindWinningMove(Letter letterToPlay, Letter[] grid)
-    //{
+    int WinOrRandomMove()
+    {
+        int winningMove = FindWinningMove(computerLetter, letterGrid);
+        if (winningMove < 0) return RandomMove();
+        return winningMove;
+    }
 
+    int FindWinningMove(Letter letterToPlay, Letter[] grid)
+    {
+        for (int line = 0; line <= 8; line++)
+        {
+            if (CheckLineForWin(letterToPlay, grid, line, out int move)) return move;
+        }
+        return -1;
+    }
 
-    //    return -1; // Temporary
-    //}
+    bool CheckLineForWin(Letter letterToPlay, Letter[] grid, int lineNum, out int move)
+    {
+        List<int> boxes = new List<int>();
+        switch (lineNum)
+        {
+            case 0:
+                boxes.Add(0);
+                boxes.Add(1);
+                boxes.Add(2);
+                break;
+            case 1:
+                boxes.Add(3);
+                boxes.Add(4);
+                boxes.Add(5);
+                break;
+            case 2:
+                boxes.Add(6);
+                boxes.Add(7);
+                boxes.Add(8);
+                break;
+            case 3:
+                boxes.Add(0);
+                boxes.Add(3);
+                boxes.Add(6);
+                break;
+            case 4:
+                boxes.Add(1);
+                boxes.Add(4);
+                boxes.Add(7);
+                break;
+            case 5:
+                boxes.Add(2);
+                boxes.Add(5);
+                boxes.Add(8);
+                break;
+            case 6:
+                boxes.Add(0);
+                boxes.Add(1);
+                boxes.Add(2);
+                break;
+            case 7:
+                boxes.Add(0);
+                boxes.Add(4);
+                boxes.Add(8);
+                break;
+            case 8:
+                boxes.Add(2);
+                boxes.Add(4);
+                boxes.Add(6);
+                break;
+            default:
+                move = -1;
+                return false;
+        }
+
+        int goodBoxes = 0;
+        int emptyBoxes = 0;
+        int theEmptyBox = -1;
+        foreach (int box in boxes)
+        {
+            if (grid[box] == letterToPlay) goodBoxes++;
+            if (grid[box] == Letter.Blank)
+            {
+                emptyBoxes++;
+                theEmptyBox = box;
+            }
+        }
+        
+        if (goodBoxes == 2 && emptyBoxes == 1)
+        {
+            move = theEmptyBox;
+            return true;
+        }
+        move = -1;
+        return false;
+    }
 }
