a65187c3bed12c5ed907d3f7672eac92336abc7f
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-02-17 10:05:30 -0600

    Add an Easter egg

---
+++ TicTacToe/Assets/Scripts/ToggleVisible.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToggleVisible : MonoBehaviour
+{
+    SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = transform.gameObject.GetComponent<SpriteRenderer>();
+        if (sr == null)
+        {
+            Debug.LogError("ToggleVisible: Missing SpriteRenderer component in object \"" + transform.gameObject.name + "\"");
+            sr = transform.gameObject.AddComponent<SpriteRenderer>();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+        sr.enabled = !sr.enabled;
+    }
+}
