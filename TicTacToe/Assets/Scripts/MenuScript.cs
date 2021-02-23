af102b4a84a9d764574f63db3fe2e5486500def2
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-02-22 21:50:32 -0600

    Update menu appearance

--- TicTacToe/Assets/Scripts/MenuScript.cs
+++ TicTacToe/Assets/Scripts/MenuScript.cs
@@ -7,7 +7,7 @@ public class MenuScript : MonoBehaviour
     GameMasterScript gm;
     GameObject menuPanel;
     GameObject uiPanel;
-    GameObject difficultyButtonText;
+    Dropdown difficultyDropdown;
     GameObject playerLetterText;
 
     private void Start()
@@ -15,10 +15,9 @@ public class MenuScript : MonoBehaviour
         gm = GameObject.Find("GameMaster").GetComponent<GameMasterScript>();
         menuPanel = GameObject.Find("MenuPanel");
         uiPanel = GameObject.Find("uiPanel");
-        difficultyButtonText = GameObject.Find("DifficultyButtonText");
+        difficultyDropdown = GameObject.Find("DifficultyDropdown").GetComponent<Dropdown>();
         playerLetterText = GameObject.Find("PlayerLetterText");
         uiPanel.SetActive(false);
-        difficultyButtonText.GetComponent<Text>().text = "DIFFICULTY: " + gm.difficulty;
     }
 
     public void ExitToMenu()
@@ -40,9 +39,7 @@ public class MenuScript : MonoBehaviour
 
     public void ChangeDifficulty()
     {
-        GameDifficulty newDifficulty = (GameDifficulty)((int)(gm.difficulty + 1) % 5);
-        gm.difficulty = newDifficulty;
-        difficultyButtonText.GetComponent<Text>().text = "DIFFICULTY: " + newDifficulty;
+        gm.difficulty = (GameDifficulty)difficultyDropdown.value;
     }
 
     public void UpdatePlayerLetter(Letter newLetter)
