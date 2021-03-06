4f488ad0c9b8f540e2c752749c8c0e3f891debce
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-03-05 23:35:21 -0600

    Add cursor position detection

---
+++ RedLightGreenLight/Assets/Scripts/TestFollowCursor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class TestFollowCursor : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z += 1;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
