f9ee07c77c5cef3eb81a7329c138225a72dcf9ea
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-03-07 23:47:51 -0600

    Add player facing

---
+++ RedLightGreenLight/Assets/Scripts/PlayerVision.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerVision : MonoBehaviour
+{
+    public string testFacingDirection;
+
+    void Update()
+    {
+        float horz = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+        float vert = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+        float horzMag = Math.Abs(horz);
+        float vertMag = Math.Abs(vert);
+        if (vertMag > horzMag)
+        {
+            if (vert >= 0) testFacingDirection = "North";
+            else testFacingDirection = "South";
+        }
+        else
+        {
+            if (horz >= 0) testFacingDirection = "East";
+            else testFacingDirection = "West";
+        }
+    }
+}
