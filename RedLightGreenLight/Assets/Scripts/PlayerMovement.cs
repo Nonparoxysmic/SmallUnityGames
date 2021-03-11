9b57cd343db870bb03468099082442487e064d0a
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-03-10 22:12:51 -0600

    Fix imprecision in grid movement

--- RedLightGreenLight/Assets/Scripts/PlayerMovement.cs
+++ RedLightGreenLight/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,13 @@ public class PlayerMovement : MonoBehaviour
 {
     const float WalkSpeed = 2.5f;
 
-    GameClock gameClock;
     [HideInInspector] public Vector3Int currentTilePos;
+    [HideInInspector] public bool isMoving;
+    [HideInInspector] public Vector3 targetPos;
 
-    bool isMoving;
-    int moveCooldown;
-    int deltaX;
-    int deltaY;
+    GameClock gameClock;
+    Vector2Int moveInput;
+    bool movingFrame;
 
     void Awake()
     {
@@ -22,35 +22,49 @@ public class PlayerMovement : MonoBehaviour
     {
         gameClock.onPlayerTick.AddListener(PlayerUpdate);
         currentTilePos = new Vector3Int((int)Math.Floor(transform.position.x), (int)Math.Floor(transform.position.y), 0);
-        transform.position = new Vector3(currentTilePos.x + 0.5f, currentTilePos.y + 0.5f, 0);
+        targetPos = new Vector3(currentTilePos.x + 0.5f, currentTilePos.y + 0.5f, 0);
+        transform.position = targetPos;
     }
 
     void Update()
     {
-        if (isMoving)
+        if (!transform.position.Equals(targetPos))
         {
+            Vector3 difference = targetPos - transform.position;
+            Vector3 step = difference / difference.magnitude * WalkSpeed * Time.deltaTime;
+            if (step.magnitude < difference.magnitude)
+            {
+                transform.position += step;
+            }
+            else
+            {
+                transform.position = targetPos;
+                isMoving = false;
+            }
             currentTilePos = new Vector3Int((int)Math.Floor(transform.position.x), (int)Math.Floor(transform.position.y), 0);
-
-            float newX = transform.position.x + deltaX * WalkSpeed * Time.deltaTime;
-            float newY = transform.position.y + deltaY * WalkSpeed * Time.deltaTime;
-            transform.position = new Vector3(newX, newY, transform.position.z);
         }
     }
 
     public void PlayerUpdate()
     {
-        if (moveCooldown > 0)
+        if (movingFrame)
         {
-            moveCooldown--;
+            movingFrame = false;
             return;
         }
-        isMoving = false;
-        deltaX = (int)Input.GetAxisRaw("East") - (int)Input.GetAxisRaw("West");
-        deltaY = (int)Input.GetAxisRaw("North") - (int)Input.GetAxisRaw("South");
-        if (deltaX == 0 ^ deltaY == 0)
+        if (!transform.position.Equals(targetPos))
         {
+            transform.position = targetPos;
+            isMoving = false;
+            currentTilePos = new Vector3Int((int)Math.Floor(transform.position.x), (int)Math.Floor(transform.position.y), 0);
+        }
+        moveInput.x = (int)Input.GetAxisRaw("Horizontal");
+        moveInput.y = (int)Input.GetAxisRaw("Vertical");
+        if (moveInput.x == 0 ^ moveInput.y == 0)
+        {
+            targetPos = new Vector3(currentTilePos.x + moveInput.x + 0.5f, currentTilePos.y + moveInput.y + 0.5f, 0);
             isMoving = true;
-            moveCooldown = 1;
+            movingFrame = true;
         }
     }
 }
