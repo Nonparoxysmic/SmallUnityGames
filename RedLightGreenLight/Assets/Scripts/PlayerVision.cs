ff5aaa9ee229d11321598e3fa1f37661a1d5891b
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-03-11 21:02:10 -0600

    Add full sized field of view

--- RedLightGreenLight/Assets/Scripts/PlayerVision.cs
+++ RedLightGreenLight/Assets/Scripts/PlayerVision.cs
@@ -4,12 +4,89 @@ using UnityEngine.Tilemaps;
 
 public class PlayerVision : MonoBehaviour
 {
+    readonly int[][,] visionPatterns = new int[][,]
+        {
+            new int[,]
+                {
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 3, 2, 2, 2, 2, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2},
+                    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2}
+                },
+            new int[,]
+                {
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1},
+                    {1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1},
+                    {1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1},
+                    {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+                },
+            new int[,]
+                {
+                    {2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 2, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 2, 2, 2, 2, 3, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 2, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 2, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                    {2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0}
+                },
+            new int[,]
+                {
+                    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                    {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                    {1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1},
+                    {1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1},
+                    {1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                }
+        };
+
     PlayerMovement playerMovement;
 
     [SerializeField] Tilemap fogTilemap;
-    int currentTileX;
-    int currentTileY;
+    Vector2Int fogPos;
 
+    [SerializeField] Tile blackTile;
+    [SerializeField] Tile grayTile;
     [SerializeField] Tile whiteTile;
 
     void Awake()
@@ -19,58 +96,42 @@ public class PlayerVision : MonoBehaviour
 
     void Update()
     {
-        currentTileX = playerMovement.currentTilePos.x;
-        currentTileY = playerMovement.currentTilePos.y;
+        Vector3 mouseDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        double angle = Math.Atan2(mouseDirection.y, mouseDirection.x);
+        int facing = 1;
+        if (angle > 0.75 * Math.PI || angle < -0.75 * Math.PI)
+        {
+            facing = 3;
+        }
+        else if (angle < -0.25 * Math.PI)
+        {
+            facing = 2;
+        }
+        else if (angle > 0.25 * Math.PI)
+        {
+            facing = 0;
+        }
 
         fogTilemap.ClearAllTiles();
-        for (int x = currentTileX - 2; x <= currentTileX + 2; x++)
+        fogPos.x = playerMovement.currentTilePos.x - 7;
+        fogPos.y = playerMovement.currentTilePos.y - 7;
+        for (int x = 0; x < 15; x++)
         {
-            for (int y = currentTileY - 2; y <= currentTileY + 2; y++)
+            for (int y = 0; y < 15; y++)
             {
-                if (x == currentTileX && y == currentTileY)
+                if (visionPatterns[facing][x, y] == 0)
                 {
-                    fogTilemap.SetTile(new Vector3Int(x, y, 0), null);
+                    fogTilemap.SetTile(new Vector3Int(fogPos.x + x, fogPos.y + y, 0), blackTile);
                 }
-                else
+                else if (visionPatterns[facing][x, y] == 1)
                 {
-                    fogTilemap.SetTile(new Vector3Int(x, y, 0), whiteTile);
+                    fogTilemap.SetTile(new Vector3Int(fogPos.x + x, fogPos.y + y, 0), grayTile);
+                }
+                else if (visionPatterns[facing][x, y] == 2)
+                {
+                    fogTilemap.SetTile(new Vector3Int(fogPos.x + x, fogPos.y + y, 0), whiteTile);
                 }
             }
         }
-
-        float horz = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
-        float vert = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
-        float horzMag = Math.Abs(horz);
-        float vertMag = Math.Abs(vert);
-        int facingX = 0;
-        int facingY = 0;
-        if (vertMag > horzMag)
-        {
-            if (vert >= 0)
-            {
-                // Facing North
-                facingY++;
-            }
-            else
-            {
-                // Facing South
-                facingY--;
-            }
-        }
-        else
-        {
-            if (horz >= 0)
-            {
-                // Facing East
-                facingX++;
-            }
-            else
-            {
-                // Facing West
-                facingX--;
-            }
-        }
-        fogTilemap.SetTile(new Vector3Int(currentTileX + facingX, currentTileY + facingY, 0), null);
-        fogTilemap.SetTile(new Vector3Int(currentTileX + 2 * facingX, currentTileY + 2 * facingY, 0), null);
     }
 }
