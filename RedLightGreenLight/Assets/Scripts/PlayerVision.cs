0dd97c8e7dd0f4a4addda28201664872f368ba54
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-03-14 21:31:00 -0500

    Improve ray casting

--- RedLightGreenLight/Assets/Scripts/PlayerVision.cs
+++ RedLightGreenLight/Assets/Scripts/PlayerVision.cs
@@ -83,7 +83,9 @@ public class PlayerVision : MonoBehaviour
     PlayerMovement playerMovement;
 
     [SerializeField] Tilemap fogTilemap;
-    Vector2Int fogPos;
+    [SerializeField] Tilemap wallTilemap;
+    Vector3Int fogArrayTilePos;
+    Vector3Int playerTilePos;
 
     [SerializeField] Tile blackTile;
     [SerializeField] Tile grayTile;
@@ -102,47 +104,60 @@ public class PlayerVision : MonoBehaviour
     void Update()
     {
         Vector3 mouseDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        double angle = Math.Atan2(mouseDirection.y, mouseDirection.x);
+        double angle = Math.Atan2(mouseDirection.y, mouseDirection.x) / Math.PI;
         int facing = 1;
-        if (angle > 0.75 * Math.PI || angle < -0.75 * Math.PI)
-        {
-            facing = 3;
-        }
-        else if (angle < -0.25 * Math.PI)
-        {
-            facing = 2;
-        }
-        else if (angle > 0.25 * Math.PI)
-        {
-            facing = 0;
-        }
+        if (angle > 0.75 || angle < -0.75) facing = 3;
+        else if (angle <= -0.25) facing = 2;
+        else if (angle >= 0.25) facing = 0;
+
 
         fogTilemap.ClearAllTiles();
-        fogPos.x = playerMovement.currentTilePos.x - 7;
-        fogPos.y = playerMovement.currentTilePos.y - 7;
+        playerTilePos = playerMovement.currentTilePos;
+        fogArrayTilePos = playerTilePos + new Vector3Int(-7, -7, 0);
         for (int x = 0; x < 15; x++)
         {
             for (int y = 0; y < 15; y++)
             {
-                if (x == 7 && y == 7) continue;
-                float distance = (float)Math.Sqrt(Math.Pow(x - 7, 2) + Math.Pow(y - 7, 2));
-                Vector3 originPos = new Vector3(playerMovement.currentTilePos.x + 0.5f, playerMovement.currentTilePos.y + 0.5f, 0);
-                RaycastHit2D hit = Physics2D.Raycast(originPos, new Vector3(x - 7, y - 7, 0), distance);
-                if (hit.collider == null) continue;
-
-                if (visionPatterns[facing][x, y] == 0)
+                Vector3Int lookTilePos = fogArrayTilePos + new Vector3Int(x, y, 0);
+                if (x == 7 && y == 7)
                 {
-                    fogTilemap.SetTile(new Vector3Int(fogPos.x + x, fogPos.y + y, 0), blackTile);
+                    fogTilemap.SetTile(lookTilePos, grayTile);
+                    continue;
                 }
-                else if (visionPatterns[facing][x, y] == 1)
+                Vector3 ray = lookTilePos - playerTilePos;
+                Vector3 playerPos = playerTilePos + new Vector3(0.5f, 0.5f, 0);
+                int steps = (int)Math.Max(Math.Abs(ray.x), Math.Abs(ray.y));
+                Vector3Int stepTilePos;
+                for (int i = 1; i <= steps; i++)
                 {
-                    fogTilemap.SetTile(new Vector3Int(fogPos.x + x, fogPos.y + y, 0), grayTile);
-                }
-                else if (visionPatterns[facing][x, y] == 2)
-                {
-                    fogTilemap.SetTile(new Vector3Int(fogPos.x + x, fogPos.y + y, 0), whiteTile);
+                    Vector3 stepPos = playerPos + ray * i / steps;
+                    stepTilePos = new Vector3Int((int)Math.Floor(stepPos.x), (int)Math.Floor(stepPos.y), 0);
+                    if (stepTilePos == playerTilePos) break;
+                    if (wallTilemap.GetTile(stepTilePos) == null)
+                    {
+                        fogTilemap.SetTile(stepTilePos, whiteTile);
+                    }
+                    else
+                    {
+                        fogTilemap.SetTile(stepTilePos, blackTile);
+                        break;
+                    }
                 }
             }
         }
+
+
+        //        if (visionPatterns[facing][x, y] == 0)
+        //        {
+        //            fogTilemap.SetTile(new Vector3Int(fogPos.x + x, fogPos.y + y, 0), blackTile);
+        //        }
+        //        else if (visionPatterns[facing][x, y] == 1)
+        //        {
+        //            fogTilemap.SetTile(new Vector3Int(fogPos.x + x, fogPos.y + y, 0), whiteTile);
+        //        }
+        //        else if (visionPatterns[facing][x, y] == 2)
+        //        {
+        //            fogTilemap.SetTile(new Vector3Int(fogPos.x + x, fogPos.y + y, 0), whiteTile);
+        //        }
     }
 }
