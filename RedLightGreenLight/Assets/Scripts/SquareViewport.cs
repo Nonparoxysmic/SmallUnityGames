299d98a2667fcf7e274b7192630cf99f45269bb1
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-03-16 21:36:03 -0500

    Implement fixed aspect ratio for viewable area

---
+++ RedLightGreenLight/Assets/Scripts/SquareViewport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class SquareViewport : MonoBehaviour
+{
+    Camera cameraComponent;
+    int previousScreenHeight;
+    int previousScreenWidth;
+
+    void Awake()
+    {
+        cameraComponent = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Screen.height != previousScreenHeight || Screen.width != previousScreenWidth)
+        {
+            float newViewportWidth = Screen.height / (float)Screen.width;
+            float newViewportPosX = (Screen.width - Screen.height) / (2.0f * Screen.width);
+            cameraComponent.rect = new Rect(newViewportPosX, 0, newViewportWidth, 1);
+            previousScreenHeight = Screen.height;
+            previousScreenWidth = Screen.width;
+        }
+    }
+}
