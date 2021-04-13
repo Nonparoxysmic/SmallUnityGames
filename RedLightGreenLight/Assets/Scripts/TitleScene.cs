d6e2cc5a682d61389eeddb042b5756b24b87ebae
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-04-12 23:43:37 -0500

    Add title scene transition

---
+++ RedLightGreenLight/Assets/Scripts/TitleScene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour
+{
+    [SerializeField] UnityEngine.Object mainMenuScene;
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(mainMenuScene.name);
+        }
+    }
+}
