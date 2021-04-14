adb2cf149f5903de583b14f25b18726c28865d1d
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-04-13 23:38:17 -0500

    Add main menu scene transitions

---
+++ RedLightGreenLight/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] UnityEngine.Object gameScene;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameScene.name);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
