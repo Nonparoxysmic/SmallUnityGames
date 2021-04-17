c1811f7fb9206afd940a513f1a79ca29d9910746
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-04-16 23:13:28 -0500

    Add audio player

---
+++ RedLightGreenLight/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public float secondsToFade;
+    AudioSource audioSource;
+    bool fadingOut;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (fadingOut)
+        {
+            if (audioSource.volume > 0)
+            {
+                if (secondsToFade > 0)
+                {
+                    audioSource.volume -= Time.deltaTime / secondsToFade;
+                    audioSource.volume = Math.Max(0, audioSource.volume);
+                }
+                else audioSource.volume = 0;
+            }
+            else
+            {
+                fadingOut = false;
+                audioSource.Stop();
+            }
+        }
+    }
+
+    public void PlayMusic()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.volume = 1;
+            audioSource.Play();
+        }
+    }
+
+    public void FadeOutMusic()
+    {
+        fadingOut = true;
+    }
+}
--- RedLightGreenLight/Assets/Scripts/MainMenu.cs
+++ RedLightGreenLight/Assets/Scripts/MainMenu.cs
@@ -5,14 +5,26 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] UnityEngine.Object gameScene;
+    GameObject audioPlayerObject;
 
     void Awake()
     {
         Time.timeScale = 1;
+        audioPlayerObject = GameObject.Find("Audio Player");
+        if (audioPlayerObject != null)
+        {
+            AudioPlayer audioPlayer = audioPlayerObject.GetComponent<AudioPlayer>();
+            audioPlayer.PlayMusic();
+        }
     }
 
     public void PlayGame()
     {
+        if (audioPlayerObject != null)
+        {
+            AudioPlayer audioPlayer = audioPlayerObject.GetComponent<AudioPlayer>();
+            audioPlayer.FadeOutMusic();
+        }
         SceneManager.LoadScene(gameScene.name);
     }
 
