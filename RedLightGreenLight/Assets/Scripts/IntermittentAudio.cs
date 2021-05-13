6abfbe8ca92474a7e55d63417cb81d5f3dbe7f6e
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-05-12 23:57:36 -0500

    Add intermittent background audio

---
+++ RedLightGreenLight/Assets/Scripts/IntermittentAudio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class IntermittentAudio : MonoBehaviour
+{
+    public float maxWaitSeconds;
+    public float minWaitSeconds;
+
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        if (audioSource != null) StartCoroutine(AudioLoop());
+    }
+
+    IEnumerator AudioLoop()
+    {
+        while (true)
+        {
+            if (minWaitSeconds > maxWaitSeconds)
+            {
+                float f = minWaitSeconds;
+                minWaitSeconds = maxWaitSeconds;
+                maxWaitSeconds = f;
+            }
+            audioSource.Play();
+            float nextInterval = audioSource.clip.length + UnityEngine.Random.Range(Math.Max(0, minWaitSeconds), Math.Max(0, maxWaitSeconds));
+            yield return new WaitForSecondsRealtime(nextInterval);
+        }
+    }
+}
