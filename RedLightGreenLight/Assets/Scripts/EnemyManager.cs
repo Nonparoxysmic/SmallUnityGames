6312ed24d389966232d27ea1c61ab3c58124521d
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-05-25 23:47:32 -0500

    Add progress report to game over screen

--- RedLightGreenLight/Assets/Scripts/EnemyManager.cs
+++ RedLightGreenLight/Assets/Scripts/EnemyManager.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class EnemyManager : MonoBehaviour
 {
+    public string timeElapsed;
     public UnityEvent onPlayerCaught;
     public UnityEvent onPlayerWin;
 
@@ -13,7 +15,10 @@ public class EnemyManager : MonoBehaviour
     AudioSource deathAudio;
     GameMenu gameMenu;
     Text gameOverText;
+    MirrorManager mirrorManager;
     PlayerVision playerVision;
+    Text progressText;
+    Stopwatch stopwatch;
 
     void Awake()
     {
@@ -23,14 +28,28 @@ public class EnemyManager : MonoBehaviour
         onPlayerWin.AddListener(PlayerWin);
         gameMenu = GameObject.Find("Canvas").GetComponent<GameMenu>();
         gameOverText = GameObject.Find("GameOverText").GetComponent<Text>();
+        mirrorManager = GameObject.Find("MirrorManager").GetComponent<MirrorManager>();
         playerVision = GameObject.Find("Player").GetComponent<PlayerVision>();
+        progressText = GameObject.Find("ProgressText").GetComponent<Text>();
+        stopwatch = new Stopwatch();
+        stopwatch.Start();
         deathAudio = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+        if (stopwatch.IsRunning)
+        {
+            SetProgressText();
+        }
     }
 
     void PlayerCaught()
     {
         playerIsCaught = true;
         Time.timeScale = 0;
+        stopwatch.Stop();
+        SetProgressText();
         playerVision.SetVision(false);
         gameMenu.StartButtonEnableCountdown();
         deathAudio.Play();
@@ -39,8 +58,20 @@ public class EnemyManager : MonoBehaviour
     void PlayerWin()
     {
         gameOverText.text = "YOU WIN";
+        stopwatch.Stop();
+        SetProgressText();
         playerVision.TerminateBlinking();
         playerVision.SetVision(true);
         gameMenu.StartButtonEnableCountdown();
+    }
+
+    void SetProgressText()
+    {
+        progressText.text = "Mirrors Activated: " + mirrorManager.mirrorsSeen + " / " + mirrorManager.totalMirrors;
+        int seconds = (int)Math.Ceiling(stopwatch.Elapsed.TotalSeconds);
+        int minutes = seconds / 60;
+        seconds -= minutes * 60;
+        timeElapsed = seconds < 10 ? minutes + ":0" + seconds : minutes + ":" + seconds;
+        progressText.text += Environment.NewLine + "Time: " + timeElapsed;
     }
 }
--- RedLightGreenLight/Assets/Scripts/MirrorManager.cs
+++ RedLightGreenLight/Assets/Scripts/MirrorManager.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class MirrorManager : MonoBehaviour
 {
     public int mirrorsSeen;
+    public int totalMirrors;
 
     EnemyManager enemyManager;
 
     void Awake()
     {
         enemyManager = GameObject.Find("EnemyManager").GetComponent<EnemyManager>();
+        totalMirrors = transform.childCount;
     }
 
     public void MirrorSeen()
     {
         mirrorsSeen++;
-        if (mirrorsSeen == transform.childCount)
+        if (mirrorsSeen == totalMirrors)
         {
             enemyManager.onPlayerWin.Invoke();
         }
