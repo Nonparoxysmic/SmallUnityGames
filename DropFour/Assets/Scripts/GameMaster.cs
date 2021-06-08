8181e566ba3c43ce8396d5206828a426453aee59
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-06-07 23:10:29 -0500

    Add placeholder tokens

--- DropFour/Assets/Scripts/GameMaster.cs
+++ DropFour/Assets/Scripts/GameMaster.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class GameMaster : MonoBehaviour
 {
     public Sprite placeholderToken;
+    Color tokenColor = Color.red;
 
     BoardSpace[,] board;
 
@@ -22,12 +23,26 @@ public class GameMaster : MonoBehaviour
 
     void DebugPlayMove(int move)
     {
+        if (move < 0 || move > 6) return;
+        if (board[move, 5] != BoardSpace.Empty) return;
+        float yPos = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            if (board[move, i] == BoardSpace.Empty)
+            {
+                board[move, i] = BoardSpace.PlayerOne;
+                yPos = i - 2.5f;
+                break;
+            }
+        }
         GameObject debugToken = new GameObject();
         debugToken.name = "Debug Token";
-        debugToken.transform.position = new Vector3(debugToken.transform.position.x - 3 + move, debugToken.transform.position.y, debugToken.transform.position.z);
+        debugToken.transform.position = new Vector3(debugToken.transform.position.x - 3 + move, debugToken.transform.position.y + yPos, debugToken.transform.position.z);
         debugToken.transform.parent = gameObject.transform;
         SpriteRenderer sr = debugToken.AddComponent<SpriteRenderer>();
         sr.sprite = placeholderToken;
+        tokenColor = new Color(tokenColor.r, (tokenColor.g + 1) % 2, tokenColor.b);
+        sr.color = tokenColor;
     }
 }
 
