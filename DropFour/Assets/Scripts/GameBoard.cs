2e835b84727b2b8aa424502db809b3b7b462ac6e
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-06-15 23:52:37 -0500

    Start implementing board representation

--- DropFour/Assets/Scripts/GameBoard.cs
+++ DropFour/Assets/Scripts/GameBoard.cs
@@ -2,10 +2,33 @@
 
 public class GameBoard
 {
-    ulong[] bitboards;
+    //  5  13  21  29  37  45  53
+    //  4  12  20  28  36  44  52
+    //  3  11  19  27  35  43  51
+    //  2  10  18  26  34  42  50
+    //  1   9  17  25  33  41  49
+    //  0   8  16  24  32  40  48
+    readonly ulong[] bitboards;
+
+    readonly int[] nextPositions;
+    readonly int[] moveList;
+    int movesMade;
 
     public GameBoard()
     {
         bitboards = new ulong[2];
+        nextPositions = new int[] { 0, 8, 16, 24, 32, 40, 48 };
+        moveList = new int[42];
+    }
+
+    public void MakeMove(int column)
+    {
+        bitboards[movesMade & 1] |= 1U << nextPositions[column]++;
+        moveList[movesMade++] = column;
+    }
+
+    public void UnmakeLastMove()
+    {
+        bitboards[movesMade & 1] ^= 1U << --nextPositions[moveList[--movesMade]];
     }
 }
