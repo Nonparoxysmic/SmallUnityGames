178c1a5f0bed247d5762cc92f8935db600c4bc29
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-06-18 22:02:34 -0500

    Add column objects

---
+++ DropFour/Assets/Scripts/Column.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class Column : MonoBehaviour
+{
+    SpriteRenderer sr;
+    Color baseColor;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        baseColor = sr.color;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Column clicked.");
+    }
+
+    void OnMouseEnter()
+    {
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b) * 0.5f + Color.red * 0.5f;
+    }
+
+    void OnMouseExit()
+    {
+        sr.color = baseColor;
+    }
+}
