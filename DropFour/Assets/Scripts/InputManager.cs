64cc77743df6cb0e0d8c54285abedd1103a76d5e
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-06-19 19:14:03 -0500

    Add input manager

---
+++ DropFour/Assets/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    Vector3 lastMousePosition;
+    float lastHorzKey;
+    float lastVertKey;
+
+    void Start()
+    {
+        lastMousePosition = Input.mousePosition;
+        lastHorzKey = Input.GetAxisRaw("Horizontal");
+        lastVertKey = Input.GetAxisRaw("Vertical");
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != lastHorzKey)
+        {
+            // Horizontal input changed
+            
+            lastHorzKey = Input.GetAxisRaw("Horizontal");
+        }
+        if (Input.GetAxisRaw("Vertical") != lastVertKey)
+        {
+            // Vertical input changed
+
+            lastVertKey = Input.GetAxisRaw("Vertical");
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Left click
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Right click
+        }
+        if (Input.mousePosition != lastMousePosition)
+        {
+            // Mouse moved this frame
+
+            lastMousePosition = Input.mousePosition;
+        }
+    }
+}
