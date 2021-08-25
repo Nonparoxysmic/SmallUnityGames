bb8e4b362a1d578a34062d0ac963985d165e55e0
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-08-24 21:29:07 -0500

    Add input repeat for left and right keys

--- DropFour/Assets/Scripts/InputManager.cs
+++ DropFour/Assets/Scripts/InputManager.cs
@@ -3,27 +3,77 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField] float repeatDelay;
+    [SerializeField] float repeatInterval;
+
     GameMaster gm;
 
+    bool holdingLeft;
+    bool holdingRight;
+    float holdingLeftDelay;
+    float holdingRightDelay;
+    DateTime holdLeftStarted;
+    DateTime holdRightStarted;
     Vector3 lastMousePosition;
 
     void Awake()
     {
         gm = GetComponent<GameMaster>();
         lastMousePosition = Vector3.positiveInfinity;
+        repeatInterval = Math.Max(repeatInterval, 0.05f);
     }
 
     void Update()
     {
+        bool pressedLeft = false;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            if (holdingLeft)
+            {
+                if ((DateTime.UtcNow - holdLeftStarted).TotalSeconds > repeatInterval + holdingLeftDelay)
+                {
+                    pressedLeft = true;
+                    holdLeftStarted = DateTime.UtcNow;
+                    holdingLeftDelay = 0;
+                }
+            }
+            else
+            {
+                pressedLeft = true;
+                holdLeftStarted = DateTime.UtcNow;
+                holdingLeftDelay = repeatDelay;
+                holdingLeft = true;
+            }
+        }
+        else holdingLeft = false;
+
+        bool pressedRight = false;
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            if (holdingRight)
+            {
+                if ((DateTime.UtcNow - holdRightStarted).TotalSeconds > repeatInterval + holdingRightDelay)
+                {
+                    pressedRight = true;
+                    holdRightStarted = DateTime.UtcNow;
+                    holdingRightDelay = 0;
+                }
+            }
+            else
+            {
+                pressedRight = true;
+                holdRightStarted = DateTime.UtcNow;
+                holdingRightDelay = repeatDelay;
+                holdingRight = true;
+            }
+        }
+        else holdingRight = false;
+
         int horizontal = 0;
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
-        {
-            horizontal--;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
-        {
-            horizontal++;
-        }
+        if (pressedLeft) horizontal--;
+        if (pressedRight) horizontal++;
+        if (holdingLeft && holdingRight) horizontal = 0;
+
         if (horizontal != 0)
         {
             gm.DirectionPressed(new Vector2Int(horizontal, 0));
