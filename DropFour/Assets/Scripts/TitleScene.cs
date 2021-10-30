98bc69ce119cb472822f7cb0058dd1e3dd5c4c0e
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2021-10-29 21:21:39 -0500

    Add scenes for title screen and main menu

---
+++ DropFour/Assets/Scripts/TitleScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour
+{
+    [SerializeField] string nextSceneName;
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
