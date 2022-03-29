54d4bc9c9255dce3be33d1e1e61e86c70d9beeab
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-03-28 21:29:55 -0500

    Continue developing collision handling

--- TileSandbox/Assets/Scripts/PlayerController.cs
+++ TileSandbox/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    static int subunitsPerUnit = 16;
+    static float subunitSize;
+
     [SerializeField] float speed;
 
+    Vector3 move;
     Vector3 moveInput;
+
+    void Start()
+    {
+        subunitSize = 1.0f / subunitsPerUnit;
+    }
 
     void Update()
     {
@@ -13,7 +22,20 @@ public class PlayerController : MonoBehaviour
         if (moveInput.x != 0 || moveInput.y != 0)
         {
             moveInput /= moveInput.magnitude;
-            transform.position += speed * Time.deltaTime * moveInput;
+            move = speed * Time.deltaTime * moveInput;
+            move.x = NonzeroFloor(move.x, subunitSize);
+            move.y = NonzeroFloor(move.y, subunitSize);
+            transform.position += move;
         }
+    }
+
+    float NonzeroFloor(float number, float significance)
+    {
+        if (number == 0) { return 0; }
+        if (Mathf.Sign(number) != Mathf.Sign(significance))
+        {
+            significance *= -1;
+        }
+        return significance * Mathf.Max(Mathf.Floor(number / significance), 1);
     }
 }
