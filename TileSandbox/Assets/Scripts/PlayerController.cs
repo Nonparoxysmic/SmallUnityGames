44ed0fccc6981bc5639cc2ec0e10808fdd47c543
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-03-30 21:41:12 -0500

    Determine player facing direction

--- TileSandbox/Assets/Scripts/PlayerController.cs
+++ TileSandbox/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int direction;
+
     [SerializeField] float speed;
+
+    static readonly int[,] directions = new int[,] { { 5, 6, 7 }, { 4, -1, 0 }, { 3, 2, 1 } };
 
     Vector3 moveInput;
 
@@ -12,8 +16,16 @@ public class PlayerController : MonoBehaviour
         moveInput.y = (int)Input.GetAxisRaw("Vertical");
         if (moveInput.x != 0 || moveInput.y != 0)
         {
+            direction = Direction(moveInput.x, moveInput.y);
             moveInput /= moveInput.magnitude;
             transform.position += speed * Time.deltaTime * moveInput;
         }
+    }
+
+    static int Direction(float x, float y)
+    {
+        int col = (int)x + 1;
+        int row = (int)y + 1;
+        return directions[col, row];
     }
 }
