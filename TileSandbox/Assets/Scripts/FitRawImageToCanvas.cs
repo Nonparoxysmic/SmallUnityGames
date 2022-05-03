209640e5ddc0c993ffc6c8d36ce543a49e654db3
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-05-02 22:57:14 -0500

    Add some error handling

--- TileSandbox/Assets/Scripts/FitRawImageToCanvas.cs
+++ TileSandbox/Assets/Scripts/FitRawImageToCanvas.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FitRawImageToCanvas : MonoBehaviour
 {
@@ -11,8 +12,41 @@ public class FitRawImageToCanvas : MonoBehaviour
 
     void Start()
     {
+        if (GetComponent<RawImage>() == null)
+        {
+            LogErrorAndDisableComponent("GameObject is not a Raw Image.");
+            return;
+        }
+        if (transform.parent == null)
+        {
+            LogErrorAndDisableComponent("No parent GameObject.");
+            return;
+        }
+        if (transform.parent.GetComponent<Canvas>() == null)
+        {
+            LogErrorAndDisableComponent("Parent GameObject is not a Canvas.");
+            return;
+        }
+
         canvasTransform = transform.parent.GetComponent<RectTransform>();
         rawImageTransform = GetComponent<RectTransform>();
+
+        if (rawImageTransform == null)
+        {
+            LogErrorAndDisableComponent("Missing or unavailable RectTransform.");
+            return;
+        }
+        if (canvasTransform == null)
+        {
+            LogErrorAndDisableComponent("Missing or unavailable parent RectTransform.");
+            return;
+        }
+    }
+
+    void LogErrorAndDisableComponent(string message)
+    {
+        Debug.LogError(name + ": " + nameof(FitRawImageToCanvas) + ": " + message);
+        enabled = false;
     }
 
     void Update()
