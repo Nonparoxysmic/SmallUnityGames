7befe37b06437cd0738363601e2517844ba9a7af
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-05-31 23:39:06 -0500

    Add some tilemap code

--- TileSandbox/Assets/Scripts/WorldManager.cs
+++ TileSandbox/Assets/Scripts/WorldManager.cs
@@ -5,6 +5,7 @@ public class WorldManager : MonoBehaviour
 {
     [SerializeField] Tilemap backgroundTilemap;
     [SerializeField] Tilemap collisionTilemap;
+    [SerializeField] Tile collisionTile;
 
     void Start()
     {
@@ -17,6 +18,25 @@ public class WorldManager : MonoBehaviour
         {
             this.Error("Collision Tilemap reference not set in Inspector.");
             return;
+        }
+        if (collisionTile is null)
+        {
+            this.Error("Collision Tile reference not set in Inspector.");
+            return;
+        }
+    }
+
+    void SetBackgroundTile(int x, int y, Tile tile, bool collision)
+    {
+        Vector3Int position = new Vector3Int(x, y, 0);
+        backgroundTilemap.SetTile(position, tile);
+        if (collision)
+        {
+            collisionTilemap.SetTile(position, collisionTile);
+        }
+        else
+        {
+            collisionTilemap.SetTile(position, null);
         }
     }
 }
