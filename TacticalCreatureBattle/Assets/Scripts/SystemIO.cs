cc15050852b3ab9b9e3b2e1f991482af5dd39383
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-10-24 21:42:14 -0500

    Add class for reading and writing files

---
+++ TacticalCreatureBattle/Assets/Scripts/SystemIO.cs
@@ -0,0 +1,11 @@
+using System.IO;
+using UnityEngine;
+
+public static class SystemIO
+{
+    //static string _creatureDataFolderPath = Application.persistentDataPath + "/creature";
+    //static string _worldDataFolderPath = Application.persistentDataPath + "/world";
+
+    // TODO: Read data from and write data to files, and make that data
+    // available to components like WorldLoader and CreatureData.
+}
