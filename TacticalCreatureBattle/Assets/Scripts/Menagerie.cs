092c6f401ca1661e162f596ddc4c1b403fced201
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-12-14 23:19:15 -0600

    Add component to carry and create creatures

---
+++ TacticalCreatureBattle/Assets/Scripts/Menagerie.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menagerie : MonoBehaviour
+{
+    [SerializeField] CreatureStats[] _prebuiltCreatures;
+
+    public static CreatureStats[] ComputerTeam { get; private set; }
+    public static CreatureStats[] HumanTeam { get; private set; }
+
+    static Menagerie _instance;
+
+    void Awake()
+    {
+        _instance = this;
+        if (_prebuiltCreatures == null)
+        {
+            this.Error("Serialized field is null.");
+            return;
+        }
+        if (_prebuiltCreatures.Length == 0)
+        {
+            this.Error($"No prebuilt creatures in {typeof(Menagerie)} array.");
+            return;
+        }
+        for (int i = 0; i < _prebuiltCreatures.Length; i++)
+        {
+            if (_prebuiltCreatures[i] == null)
+            {
+                this.Error($"Missing reference in inspector: \"{nameof(_prebuiltCreatures)}\" array.");
+                return;
+            }
+        }
+        // TODO: Load any saved creatures from Application.persistentDataPath folder.
+        // TODO: Initialize the team arrays.
+    }
+
+    public static CreatureStats[] GetPrebuiltCreatures()
+    {
+        List<CreatureStats> creatureStats = new List<CreatureStats>();
+        foreach (CreatureStats cs in _instance._prebuiltCreatures)
+        {
+            creatureStats.Add(Instantiate(cs));
+        }
+        return creatureStats.ToArray();
+    }
+
+    public static GameObject CreateUnit(CreatureStats creatureStats, Battle battle, Transform parent = null)
+    {
+        GameObject unit = new GameObject
+        {
+            name = $"Unit: {creatureStats.CreatureName}"
+        };
+        if (parent != null)
+        {
+            unit.transform.parent = parent;
+        }
+        UnitController uc = unit.AddComponent<UnitController>();
+        uc.Initialize(creatureStats, battle);
+        return unit;
+    }
+}
