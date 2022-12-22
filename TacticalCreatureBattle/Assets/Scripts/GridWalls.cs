13ef80d439005ebc949049252f6016da7a828dc9
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-12-21 22:08:40 -0600

    Add component for grid based collision

---
+++ TacticalCreatureBattle/Assets/Scripts/GridWalls.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class GridWalls : MonoBehaviour
+{
+    public Color WallColor { get; set; }
+
+    Grid _grid;
+
+    void Awake()
+    {
+        if (transform.parent == null)
+        {
+            this.Error($"{typeof(GridWalls)} requires parent Grid.");
+            return;
+        }
+        _grid = transform.parent.GetComponent<Grid>();
+        if (_grid == null)
+        {
+            this.Error($"{typeof(GridWalls)} requires parent Grid.");
+            return;
+        }
+        if (_grid.cellLayout != GridLayout.CellLayout.Rectangle)
+        {
+            this.Error($"{typeof(GridWalls)} only supports Rectangle grids.");
+            return;
+        }
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+
+    }
+
+    public void Click(Vector3 worldPosition)
+    {
+
+    }
+}
