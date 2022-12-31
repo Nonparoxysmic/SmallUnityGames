3104003a361cf27752f4c5e053850bc7d7451498
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2022-12-30 23:20:10 -0600

    Add a method to identify the nearest grid line segment

--- TacticalCreatureBattle/Assets/Scripts/GridWalls.cs
+++ TacticalCreatureBattle/Assets/Scripts/GridWalls.cs
@@ -37,4 +37,33 @@ public class GridWalls : MonoBehaviour
     {
 
     }
+
+    /// <summary>
+    /// Gets the coordinates of the grid line segment closest to the cell position.
+    /// </summary>
+    /// <returns>
+    /// A tuple of two <seealso cref="Vector3"/>, representing the cell position 
+    /// of the start of the line segment, and the cell position of the end 
+    /// of the line segment, in that order.
+    /// </returns>
+    /// <param name="cellPosition">A cell position.</param>
+    /// <returns></returns>
+    (Vector3, Vector3) NearestGridLineSegment(Vector3 cellPosition)
+    {
+        float roundedX = Mathf.Round(cellPosition.x);
+        float roundedY = Mathf.Round(cellPosition.y);
+        if (Mathf.Abs(cellPosition.x - roundedX) < Mathf.Abs(cellPosition.y - roundedY))
+        {
+            return
+                (
+                    new Vector3(roundedX, Mathf.Floor(cellPosition.y), 0),
+                    new Vector3(roundedX, Mathf.Floor(cellPosition.y) + 1, 0)
+                );
+        }
+        return
+            (
+                new Vector3(Mathf.Floor(cellPosition.x), roundedY, 0),
+                new Vector3(Mathf.Floor(cellPosition.x) + 1, roundedY, 0)
+            );
+    }
 }
